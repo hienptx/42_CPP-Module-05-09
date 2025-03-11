67592209bf1f7a3a85d5b2a924f069d7fedc3af3
mafalda <https://github.com/hienptx>
2025-03-11 16:55:59 +0100

    ex02

--- cpp08/ex02/includes/MutantStack.hpp
+++ cpp08/ex02/includes/MutantStack.hpp
@@ -3,10 +3,26 @@
 
 #include <stack>
 #include <iterator>
+#include <cstddef>
 
-class MutantStack : public std::stack
+// using hear is equivalent to typedef
+
+template <class T>
+class MutantStack : public std::stack<T>
 {
+    public:
+        struct iterator {
+                typedef T   value_type = T;;
+                typedef Distance difference_type = std::ptrdiff_t;
+                // using difference_type = std::prtdiff_t;
+                typedef Pointer pointer = T*;
+                typedef Reference reference = &T;
+                typedef Category iterator_category = std::forward_iterator_tag;
 
+                iterator(pointer ptr);
+            private:
+                pointer m_ptr;
+        };
 };
 
 #endif
