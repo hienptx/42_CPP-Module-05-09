4db0a3c9747ddec6779f5bc578de7cab9610eb1f
mafalda <https://github.com/hienptx>
2025-03-12 22:05:11 +0100

    cpp08: ex02

--- cpp08/README.md
+++ cpp08/README.md
@@ -1,6 +1,16 @@
 ## CPP Module 08: Generic programing
-Generic programming is when a single algorithm is written to work on many data types. As programmers, we only have to write the algorithm once: the compiler takes care of generating the code for each data type, reducing the need to write duplicate code.
-Topics: Templated containers, iterators, algorithm
+Generic programming is when a single algorithm is written to work on many data types. 
+As programmers, we only have to write the algorithm once: the compiler takes care of 
+generating the code for each data type, reducing the need to write duplicate code.
+Topics: Templated containers (vector) , iterators, algorithm
+
 # ex00: Easy find
-# ex01:
-# ex02:
+    - Return a pointer to the value to find in a vector of integers
+# ex01: Span
+    - Create span of integers
+    - Add number to the array
+    - Find shortest and longest span of the integer vector
+# ex02: MutantStack
+    - Create iterator functionality for std::stack
+
+[Source](https://internalpointers.com/post/writing-custom-iterators-modern-cpp)
--- cpp08/ex02/includes/MutantStack.hpp
+++ cpp08/ex02/includes/MutantStack.hpp
@@ -5,21 +5,55 @@
 #include <iterator>
 #include <cstddef>
 
-// using hear is equivalent to typedef
+// "using" here is equivalent to "typedef <tag>"
+// Tags are used to select the most efficient algorithm if your container is passed to one of 
+// the Standard Library functions from the <algorithm> library. Wrong tags mean sub-optimal performance! 
 
+/* C++ expects some properties/tags from an iterator:
+iterator_category   — one of the six iterator categories we have seen above. 
+                    The full list is available here. The std::forward_iterator_tag tag is what we need;
+difference_type     — a signed integer type that can be used to identify distance between iterator steps. 
+                    Our iterator is basically a wrapper around a pointer and leverages pointer arithmetic, 
+                    so the default std::ptrdiff_t is a good choice;
+value_type          — the type the iterator iterates over. T in our case;
+pointer             — defines a pointer to the type iterated over. T* in our case;
+reference           — defines a reference to the type iterated over. T& in our case;
+*/
+
+// All iterators must be constructible, copy-constructible, copy-assignable, destructible and swappable.
 template <class T>
 class MutantStack : public std::stack<T>
 {
     public:
         struct iterator {
-                typedef T   value_type = T;;
-                typedef Distance difference_type = std::ptrdiff_t;
-                // using difference_type = std::prtdiff_t;
-                typedef Pointer pointer = T*;
-                typedef Reference reference = &T;
-                typedef Category iterator_category = std::forward_iterator_tag;
+                using value_type = T;;
+                using difference_type = std::ptrdiff_t;
+                using pointer = T*;
+                using reference = T&;
+                using iterator_category = std::forward_iterator_tag;
 
+                // Constructor
                 iterator(pointer ptr);
+
+                // Implement operators
+                reference operator*() const;
+                pointer operator->();
+
+                // Prefix increment
+                iterator& operator++();
+                // Postfix increment
+                iterator operator++(int);
+
+                // Prefix decrement
+                iterator& operator--();
+                // Postfix decrement
+                iterator operator--(int);
+                
+                // Compare operators    
+                // These operators take only 1 param, because they are member function
+                // For non-member function, they have to take 2 params 
+                bool operator== (const iterator& other);
+                bool operator!= (const iterator& other);
             private:
                 pointer m_ptr;
         };
