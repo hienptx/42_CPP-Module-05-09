60def98b6461e748eba9f43f8d4ca1443dd1a889
Hien Pham <hipham@1-B-12.42heilbronn.de>
2025-03-19 20:20:15 +0100

    cpp09: ex00

--- cpp09/ex00/includes/BitcoinExchange.hpp
+++ cpp09/ex00/includes/BitcoinExchange.hpp
@@ -6,11 +6,36 @@
 /*   By: hipham <hipham@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/17 08:35:57 by hipham            #+#    #+#             */
-/*   Updated: 2025/03/17 08:37:38 by hipham           ###   ########.fr       */
+/*   Updated: 2025/03/19 19:14:16 by hipham           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <deque>
 #include <regex>
+
+class BitcoinExchange
+{
+	private:
+		std::deque date;
+		std::deque valuePerBtc;
+		std::deque totalValue;
+	public:	
+		BitcoinExchange();
+		BitcoinExchange(const BitcoinExchange &other);
+		BitcoinExchange &operator=(const BitcoinExchange &other);
+		~BitcoinExchange();
+};
+
+BitcoinExchange::BitcoinExchange() {}
+BitcoinExchange::BitcoinExchange(const BitcoinExchange &other)
+: date = other.date,
+valuePerBtc = other.valuePerBtc,
+totalValue = other.totalValue
+{}
+BitcoinExchange &BitcoinExchange::operator=(const BitcoinExchange &other)
+{
+	
+}
+
 
--- cpp09/ex00/srcs/main.cpp
+++ cpp09/ex00/srcs/main.cpp
@@ -6,12 +6,25 @@
 /*   By: hipham <hipham@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/25 22:31:37 by hipham            #+#    #+#             */
-/*   Updated: 2025/03/17 09:16:55 by hipham           ###   ########.fr       */
+/*   Updated: 2025/03/19 18:55:02 by hipham           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/BitcoinExchange.hpp"
 #include <fstream>
+
+/*
+$> ./btc input.txt
+2011-01-03 => 3 = 0.9
+2011-01-03 => 2 = 0.6
+2011-01-03 => 1 = 0.3
+2011-01-03 => 1.2 = 0.36
+2011-01-09 => 1 = 0.32
+Error: not a positive number.
+Error: bad input => 2001-42-42
+2012-01-11 => 1 = 7.1
+Error: too large a number.
+*/
 
 int main(int ac, char *av[])
 {
@@ -19,11 +32,19 @@ int main(int ac, char *av[])
     
     if (ac != 2)
         return std::cerr << "Error: Invalid argument.\n./btc [input.txt]\n", 0;
-    std::ifstream input(av[1]);
+    // open input.txt
+        std::ifstream input(av[1]);
     if (!input)
         return std::cerr << "Error: could not open file\n", 1;
     while (std::getline(input, line))
         std::cout << line << std::endl;
+    // open data.csv
+    std::ifstream data("data.csv");
+    if (!data)
+        return std::cerr << "Error: could not open file\n", 1;
+    // while (std::getline(data, line))
+    //     std::cout << line << std::endl;
     input.close();
+    data.close();
     return 0;
 }
